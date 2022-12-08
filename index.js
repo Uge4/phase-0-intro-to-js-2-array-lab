9dc1fb3dead35c77184dff7e40a516b29af1ca1c
eugene <eugeneholenstein@gmail.com>
2022-12-08 21:27:06 +1100

    commit tree a25ae8993af80fb884434924b02661a2d6c330f1

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Write your solution here!
+
+const cats = [`Milo`, `Otis`, `Garfield`]
+console.log(cats);
+
+function fixCats() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+    console.log(cats);
+}
+
+fixCats();
+
+function destructivelyPrependCat(){
+    cats.unshift(`Bob`)
+    console.log(cats);
+}
+
+fixCats();
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    console.log(cats);
+}
+
+fixCats();
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    console.log(cats);
+}
+
+fixCats();
+
+function appendCat(){
+    const cats2 = [...cats, `Broom`]
+    return cats2;
+    // console.log(cats2);
+    // // console.log(`test2`, cats);
+}
+
+fixCats();
+
+function prependCat(){
+    const cats3 = [`Arnold`, ...cats]
+    return cats3;
+    // console.log(cats3);
+    // console.log(`test3`, cats);
+}
+
+fixCats();
+
+function removeLastCat(){
+    const cats4 = cats.slice(0, -1);
+    return cats4;
+}
+
+function removeFirstCat(){
+    const cats5 = cats.slice(1)
+    return cats5;
+}
+
